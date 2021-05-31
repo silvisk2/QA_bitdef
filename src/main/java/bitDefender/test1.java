3061ac4b187ac7ae1504a5a260fa933ed556e2c7
silvisk2 <capraru.silviu@hotmail.com>
2021-06-01 01:00:12 +0300

    01.06.2021

---
+++ src/main/java/bitDefender/test1.java
@@ -0,0 +1,77 @@
+package bitDefender;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestListener;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class test1 implements ITestListener {
+
+    public String url = "https://central.bitdefender.com/";
+    String driverPath = "C:\\Users\\Silviu\\Desktop\\Homework_Capraru\\chromedriver.exe";
+    public WebDriver driver;
+
+
+    @BeforeTest
+    public void launchBrowser() {
+
+        System.out.println("Launching Chrome Browser");
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+
+    @Test(priority = 1)
+    public void toEmail() {
+
+        driver.findElement(By.cssSelector(".btn.primary.solid")).click();
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "https://login.bitdefender.com/central/login.html?redirect_url=https:%2F%2Fcentral.bitdefender.com%2Fdashboard&lang=en_US&ref=https:%2F%2Fcentral.bitdefender.com%2F&partner_id=com.bitdefender";
+        Assert.assertEquals(actualURL, expectedURL);
+    }
+
+    @Test(priority = 2)
+    public void enterEmail() throws InterruptedException {
+
+        WebElement email = driver.findElement(By.cssSelector("#username"));
+        email.click();
+        email.sendKeys("test+13-15@mailinator.com");
+        WebElement next = driver.findElement(By.cssSelector("#login-next"));
+        next.click();
+        Thread.sleep(3000);
+
+        String actualURL = driver.findElement(By.id("password-sign-in")).getText();
+        String expectedURL = "SIGN IN";
+        Assert.assertEquals(actualURL, expectedURL);
+    }
+
+    @Test(priority = 3)
+    public void enterWrongPass() throws InterruptedException, IOException {
+
+        driver.findElement(By.id("password_input")).sendKeys("Password1234");
+        driver.findElement(By.id("password-sign-in")).click();
+        WebElement textInvalid = driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[3]/div/div[3]"));
+        Thread.sleep(1000);
+        System.out.println(textInvalid.getText());
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("C:\\Users\\Silviu\\Desktop\\Homework_Capraru\\ScreenShots\\b.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("The screenshot is taken");
+    }
+
+}
+
+
---
+++ src/main/java/bitDefender/test2.java
@@ -0,0 +1,78 @@
+package bitDefender;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class test2 {
+
+    public String url = "https://central.bitdefender.com/";
+    String driverPath = "C:\\Users\\Silviu\\Desktop\\Homework_Capraru\\chromedriver.exe";
+    public WebDriver driver;
+
+    @BeforeTest
+    public void launchBrowser() {
+
+        System.out.println("Launching Chrome Browser");
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+    @Test(priority = 1)
+    public void toEmail() {
+
+        driver.findElement(By.cssSelector(".btn.primary.solid")).click();
+    }
+    @Test(priority = 2)
+    public void enterEmailPassword() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        WebElement email = driver.findElement(By.cssSelector("#username"));
+        email.click();
+        email.sendKeys("test+13-15@mailinator.com");
+        WebElement next = driver.findElement(By.cssSelector("#login-next"));
+        next.click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("password_input")).sendKeys("gJFjyfd$^%U7EDf");
+        driver.findElement(By.id("password-sign-in")).click();
+        long finish = System.currentTimeMillis();
+        long Total_Time = (finish - start)/1000;
+        System.out.println("Total page load time:" + Total_Time + "seconds");
+        if ( Total_Time > 5)
+            System.out.println("NOT OK");
+        else {
+            System.out.println("OK");
+        }
+    }
+    @Test(priority = 3)
+    public void continueTrial() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.id("btn_1597825507")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("ui-card:nth-of-type(1) > .align-items-start.card-header.p-1")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector(".bordered.card.clickable-card.gray.ng-star-inserted.raised > .card-header")).click();
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("C:\\Users\\Silviu\\Desktop\\Homework_Capraru\\ScreenShots\\c.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("The screenshot is taken");
+    }
+
+
+
+
+
+}
